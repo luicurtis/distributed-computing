6fb31463aa00cb122af2cac73710a8037ebf9e6c
Curtis Lui <cwlui@sfu.ca>
2022-01-25 09:38:45 -0800

    updated slurm script to run from /home/cwlui

--- assignment2/slurm_scripts/curve_area_parallel.sh
+++ assignment2/slurm_scripts/curve_area_parallel.sh
@@ -5,4 +5,4 @@
 #SBATCH --mem=1G
 #SBATCH --partition=slow
 
-srun /home/cwlui/sfuhome/cmpt431/assignment2/curve_area_parallel --nPoints 1000000000 --coeffA 3.2 --coeffB 7.1 --rSeed 37 --nThreads 8
+srun /home/cwlui/cmpt431/assignment2/curve_area_parallel --nPoints 1000000000 --coeffA 3.2 --coeffB 7.1 --rSeed 37 --nThreads 8
