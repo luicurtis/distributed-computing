a5dea2dd8b57de94101598883bcc969768587a46
Curtis Lui <cwlui@sfu.ca>
2022-02-02 13:18:03 -0800

    pull parallel output is correct and can be ran on slurm
    - need to finish other implementations in order to execute other tests
    - added shell scripts for slurm testing

---
+++ assignment3/slurm/test_page_rank_pull_parallel.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#SBATCH --cpus-per-task=8
+#SBATCH --time=02:00
+#SBATCH --mem=1G
+#SBATCH --partition=slow
+
+srun python /home/cwlui/cmpt431/assignment3/scripts/page_rank_tester.pyc  --execPath=/home/cwlui/cmpt431/assignment3/ --scriptPath=/home/cwlui/cmpt431/assignment3/scripts/page_rank_evaluator.pyc --inputPath=/scratch/input_graphs/
