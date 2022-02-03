2a3a5f41975d49ba5f65601704d947dea3ebbf78
Curtis Lui <cwlui@sfu.ca>
2022-02-03 10:09:11 -0800

    created slurm script for push parallel

---
+++ assignment3/slurm/page_rank_push_parallel.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#SBATCH --cpus-per-task=8
+#SBATCH --time=02:00
+#SBATCH --mem=1G
+#SBATCH --partition=slow
+
+srun /home/cwlui/cmpt431/assignment3/page_rank_push_parallel --nThreads 8 --nIterations 10 --inputFile /scratch/input_graphs/roadNet-CA
