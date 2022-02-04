1334273f46adb3c2b56dd5eaf164c389f42236aa
Curtis Lui <cwlui@sfu.ca>
2022-02-04 15:22:48 -0800

    tested on slurm
    - updated scripts to use cpu2

--- assignment3/slurm/page_rank_pull_parallel.sh
+++ assignment3/slurm/page_rank_pull_parallel.sh
@@ -4,5 +4,6 @@
 #SBATCH --time=02:00
 #SBATCH --mem=1G
 #SBATCH --partition=slow
+#SBATCH --nodelist=cs-cloud-02
 
 srun /home/cwlui/cmpt431/assignment3/page_rank_pull_parallel --nThreads 8 --nIterations 10 --inputFile /scratch/input_graphs/roadNet-CA
--- assignment3/slurm/test_page_rank_pull_parallel.sh
+++ assignment3/slurm/test_page_rank_pull_parallel.sh
@@ -4,5 +4,7 @@
 #SBATCH --time=02:00
 #SBATCH --mem=1G
 #SBATCH --partition=slow
+#SBATCH --nodelist=cs-cloud-02
 
-srun python /home/cwlui/cmpt431/assignment3/scripts/page_rank_tester.pyc  --execPath=/home/cwlui/cmpt431/assignment3/ --scriptPath=/home/cwlui/cmpt431/assignment3/scripts/page_rank_evaluator.pyc --inputPath=/scratch/input_graphs/
+
+srun python /home/cwlui/cmpt431/assignment3/scripts/page_rank_tester.pyc --execPath=/home/cwlui/cmpt431/assignment3/ --scriptPath=/home/cwlui/cmpt431/assignment3/scripts/page_rank_evaluator.pyc --inputPath=/scratch/input_graphs/
