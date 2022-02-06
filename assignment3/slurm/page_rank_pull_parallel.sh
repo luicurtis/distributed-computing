823d7925dfa1ea3883d51d6b4f3529fa32446127
Curtis Lui <cwlui@sfu.ca>
2022-02-06 15:41:33 -0800

    new times from slurm
    -much better performance after adding vector of mutexes

--- assignment3/slurm/page_rank_pull_parallel.sh
+++ assignment3/slurm/page_rank_pull_parallel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=4
 #SBATCH --time=02:00
 #SBATCH --mem=1G
 #SBATCH --partition=slow
--- assignment3/slurm/page_rank_push_parallel_atomic.sh
+++ assignment3/slurm/page_rank_push_parallel_atomic.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=4
 #SBATCH --time=02:00
 #SBATCH --mem=1G
 #SBATCH --partition=slow
 #SBATCH --nodelist=cs-cloud-02
 
-srun /home/cwlui/cmpt431/assignment3/page_rank_push_parallel_atomic --nThreads 8 --nIterations 10 --inputFile /scratch/input_graphs/roadNet-CA
+srun /home/cwlui/cmpt431/assignment3/page_rank_push_parallel_atomic --nThreads 4 --nIterations 10 --inputFile /scratch/input_graphs/roadNet-CA
