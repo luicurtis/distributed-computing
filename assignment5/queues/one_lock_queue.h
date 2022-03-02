e0632e61c7b818bb8de8f0c7e9fa287859a8077b
Curtis Wing-Kae Lui <cwlui@sfu.ca>
2022-03-02 11:29:40 -0800

    one lock queue passing correctness

--- assignment5/queues/one_lock_queue.h
+++ assignment5/queues/one_lock_queue.h
@@ -1,40 +1,58 @@
+#include <mutex>
+
 #include "../common/allocator.h"
 
 template <class T>
-class Node
-{
+class Node {
+ public:
+  long value;
+  Node<T>* next;
 };
 
 template <class T>
-class OneLockQueue
-{
-    CustomAllocator my_allocator_;
-public:
-    OneLockQueue() : my_allocator_()
-    {
-        std::cout << "Using OneLockQueue\n";
-    }
+class OneLockQueue {
+  Node<T>* q_head;
+  Node<T>* q_tail;
+  std::mutex mtx;
+  CustomAllocator my_allocator_;
 
-    void initQueue(long t_my_allocator_size){
-        std::cout << "Using Allocator\n";
-        my_allocator_.initialize(t_my_allocator_size, sizeof(Node<T>));
-        // Initialize the queue head or tail here
-        Node<T>* newNode = (Node<T>*)my_allocator_.newNode();
-        my_allocator_.freeNode(newNode);
-    }
+ public:
+  OneLockQueue() : my_allocator_() { std::cout << "Using OneLockQueue\n"; }
 
-    void enqueue(T value)
-    {
-    }
+  void initQueue(long t_my_allocator_size) {
+    std::cout << "Using Allocator\n";
+    my_allocator_.initialize(t_my_allocator_size, sizeof(Node<T>));
+    // Initialize the queue head or tail here
+    Node<T>* new_node = (Node<T>*)my_allocator_.newNode();
+    q_head = new_node;
+    q_tail = new_node;
+  }
 
-    bool dequeue(T *value)
-    {
-        bool ret_value = false;
-        return ret_value;
-    }
+  void enqueue(T value) {
+    mtx.lock();
+    Node<T>* new_node = (Node<T>*)my_allocator_.newNode();
+    new_node->value = value;
+    new_node->next = nullptr;
+    q_tail->next = new_node;
+    q_tail = new_node;
+    mtx.unlock();
+  }
 
-    void cleanup()
-    {
-        my_allocator_.cleanup();
+  bool dequeue(T* value) {
+    mtx.lock();
+    Node<T>* sentinel = q_head;
+    Node<T>* new_head = q_head->next;
+    if (new_head == nullptr) {
+      // queue is empty
+      mtx.unlock();
+      return false;
     }
+    *value = new_head->value;
+    q_head = new_head;
+    my_allocator_.freeNode(sentinel);
+    mtx.unlock();
+    return true;
+  }
+
+  void cleanup() { my_allocator_.cleanup(); }
 };
