c6325da5cc7d3d824112a283ec966ec30b9be676
Curtis Wing-Kae Lui <cwlui@sfu.ca>
2022-03-02 11:52:51 -0800

    init formatting, not functional code

--- assignment5/queues/non_blocking_queue.h
+++ assignment5/queues/non_blocking_queue.h
@@ -4,46 +4,36 @@
 #define SFENCE asm volatile("sfence" : : : "memory")
 
 template <class T>
-class Node
-{
-public:
+class Node {
+ public:
 };
 
 template <class T>
-class NonBlockingQueue
-{
-    CustomAllocator my_allocator_;
-public:
-    
-    NonBlockingQueue() : my_allocator_()
-    {
-        std::cout << "Using NonBlockingQueue\n";
-    }
+class NonBlockingQueue {
+  CustomAllocator my_allocator_;
 
-    void initQueue(long t_my_allocator_size){
-        std::cout << "Using Allocator\n";
-        my_allocator_.initialize(t_my_allocator_size, sizeof(Node<T>));
-        // Initialize the queue head or tail here
-        Node<T>* newNode = (Node<T>*)my_allocator_.newNode();
-        my_allocator_.freeNode(newNode);
-    }
+ public:
+  NonBlockingQueue() : my_allocator_() {
+    std::cout << "Using NonBlockingQueue\n";
+  }
 
-    void enqueue(T value)
-    {
-        // Use LFENCE and SFENCE as mentioned in pseudocode
-    }
+  void initQueue(long t_my_allocator_size) {
+    std::cout << "Using Allocator\n";
+    my_allocator_.initialize(t_my_allocator_size, sizeof(Node<T>));
+    // Initialize the queue head or tail here
+    Node<T>* newNode = (Node<T>*)my_allocator_.newNode();
+    my_allocator_.freeNode(newNode);
+  }
 
-    bool dequeue(T *value)
-    {
-        // Use LFENCE and SFENCE as mentioned in pseudocode
-        bool ret_value = false;
-        return ret_value;
-    }
+  void enqueue(T value) {
+    // Use LFENCE and SFENCE as mentioned in pseudocode
+  }
 
-    void cleanup()
-    {
-        my_allocator_.cleanup();
-    }
+  bool dequeue(T* value) {
+    // Use LFENCE and SFENCE as mentioned in pseudocode
+    bool ret_value = false;
+    return ret_value;
+  }
 
+  void cleanup() { my_allocator_.cleanup(); }
 };
-
