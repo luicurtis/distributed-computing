19c6cd4b11fdea7bbafdc4cb0334e1d1702882f5
Curtis Lui <cwlui@sfu.ca>
2022-03-08 10:42:40 -0800

    tested correctness

--- assignment5/slurm/correctness.sh
+++ assignment5/slurm/correctness.sh
@@ -5,4 +5,4 @@
 #SBATCH --mem=2G
 #SBATCH --partition=slow
 
-srun /home/cwlui/cmpt431/assignment5/non_blocking_queue_correctness --n_producers 2 --n_consumers 2 --inputFile inputs/rand_10M
+srun /home/cwlui/cmpt431/assignment5/two_lock_queue_correctness --n_producers 2 --n_consumers 2 --inputFile inputs/rand_10M
